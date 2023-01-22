63056472c8dc1cd611adccc654c655e7da79eedc
manoj singh <117524976+manojsinghkoranga@users.noreply.github.com>
2023-01-22 19:37:08 +0000

    tried

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,24 @@ import React from 'react'
 import '../styles/App.css';
 
 class App extends React.Component {
-  
+  constructor(props){
+    super(props);
+    this.state = {name: "John Doe", enrollmentNo: "12345678", age: 34}
+  }
+
+  handleClick = () => {
+    this.setState({age: this.state.age + 1});
+  }
+
+  render(){
+        return (
+            <>
+                <h1>Hello, my name is {this.state.name}</h1>
+                <p>I am age {this.state.age} old and my enrollment no is {this.state.enrollmentNo}</p>
+                <button onClick={this.handleClick}>Increase</button>
+            </>
+        )
+    }
 }
 
 
