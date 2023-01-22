3b4a8199b28d97727ec617b8af77fe25b3d22e8a
manoj singh <117524976+manojsinghkoranga@users.noreply.github.com>
2023-01-22 19:40:46 +0000

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
         return (
             <>
                 <h1>Hello, my name is {this.state.name}</h1>
-                <p>I am age {this.state.age} old and my enrollment no is {this.state.enrollmentNo}</p>
+                <p>I am {this.state.age} years old and my enrollment no is {this.state.enrollmentNo}</p>
                 <button onClick={this.handleClick}>Increase</button>
             </>
         )
